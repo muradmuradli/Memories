91c11822dcd2658bc49fe6899dec43cb337cc65e
Murad Muradli <mmuradmuradlii@gmail.com>
2023-02-10 21:54:57 +0100

    new message

--- server-2/server.js
+++ server-2/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const connectDB = require("./db/connectDB");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 
 require("dotenv").config();
 
@@ -21,6 +24,9 @@ app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload({ useTempFiles: true }));
 app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
@@ -33,6 +39,11 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts/comments", commentRouter);
+
+app.get("/", (req, res) => {
+  res.setHeader("Acess-Control-Allow-Credentials", "true");
+  res.send("API is running");
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
